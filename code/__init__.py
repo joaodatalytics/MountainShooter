a7f2d256b95bb8ad45cb367543215f2db40a2eb5
JoaoGS <joooogabrielnscmnt4@gmail.com>
2026-03-05 21:33:19 -0300

    Creating menu interations Creating ABC class Creating Factory Level 1 Bg inplementation: parallax

---
+++ code/__init__.py
@@ -0,0 +1,16 @@
+import pygame
+
+class Level:
+    pass
+
+
+class Menu:
+    pass
+
+
+class Level:
+    pass
+
+
+class Menu:
+    pass
--- code/game.py
+++ code/game.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+
+
 import pygame
 
-from code.Const import WIN_WIDTH, WIN_HEIGHT
+from code.Const import WIN_WIDTH, WIN_HEIGHT, MENU_OPTION
+from code.level import Level  # Correção 1: Importando a classe com L maiúsculo (verifique se no seu arquivo está assim)
 from code.menu import Menu
 
 
@@ -13,7 +16,30 @@ class Game:
 
     def run(self):
         while True:
-           menu = Menu(self.window)
-           menu.run()
-           pass
+            menu = Menu(self.window)
+            menu_return = menu.run()
+
+            if menu_return in [MENU_OPTION[0], MENU_OPTION[1], MENU_OPTION[2]]:
+                # Correção 2: Mudei o nome da variável para 'current_level'
+                # e instanciei a classe 'Level' corretamente.
+                current_level = Level(self.window, 'Level1', menu_return)
+
+                # Correção 3: Se não for usar o 'level_return', você pode apenas usar assim:
+                current_level.run()
+
+            elif menu_return == MENU_OPTION[4]:
+                pygame.quit()  # Close Window
+                quit()  # end pygame
+            else:
+                pass
+
+
+
+
+
+
+
+
+
+
 
