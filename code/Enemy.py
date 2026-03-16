0c765b91461831669683e1c11e04dfb0469b8bd0
JoaoGS <joooogabrielnscmnt4@gmail.com>
2026-03-15 21:42:17 -0300

    Creating Mediator Pattern. Enemies destruction

--- code/Enemy.py
+++ code/Enemy.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-from code.Const import ENTITY_SPEED, WIN_WIDTH
+from code.Const import ENTITY_SPEED
 from code.Entity import Entity
 
 
@@ -11,5 +11,4 @@ class Enemy(Entity):
 
     def move(self, ):
         self.rect.centerx -= ENTITY_SPEED[self.name]
-        if self.rect.right <= 0:
-            self.rect.left = WIN_WIDTH
+
--- code/Level.py
+++ code/Level.py
@@ -11,6 +11,8 @@ from pygame.surface import Surface
 from code.Const import WIN_HEIGHT, C_WHITE, MENU_OPTION, EVENT_ENEMY, SPAWN_TIME
 from code.Entity import Entity
 from code.EntityFactory import EntityFactory
+from code.EntityMediator import EntityMediator
+
 
 class Level:
     def __init__(self, window, name, game_mode):
@@ -48,6 +50,9 @@ class Level:
             self.level_text(14, f'fps: {clock.get_fps():.0f}', C_WHITE, (10, WIN_HEIGHT - 35))
             self.level_text(14, f'entidades: {len(self.entity_list)}', C_WHITE, (10, WIN_HEIGHT - 20))
             pygame.display.flip()
+            # Collisions
+            EntityMediator.verify_collision(entity_list=self.entity_list)
+            EntityMediator.verify_health(entity_list=self.entity_list)
             pass
 
 
